6541158cf5e314e2768ec9504f18ec07cede8fec
nircniv <ng.ta.wei@outlook.com>
2024-05-16 21:04:29 +0800

    Refactor frame processing for improved performance

--- server/app.py
+++ server/app.py
@@ -1,130 +1,62 @@
+from flask import Flask, Response
 import cv2
 import time
-import base64
 import numpy as np
-from flask import Flask, Response
-from flask_socketio import SocketIO, Namespace, emit
-from threading import Thread
+import requests
 from object_detection.handler import predict_image
 
 app = Flask(__name__)
-socketio = SocketIO(app)
 
-unprocessed_frame = None
-processed_frame = None
-
-fps = 0
-frame_count = 0
+RPI_IP = 'http://debrisx.local:8000/stream.mjpg'
+                
 start_time = time.time()
+last_fps_time = start_time
+frame_count = 0
 
-def generate_frames():
-    global fps, processed_frame
+def predicted_stream():
+    global frame_count, start_time, last_fps_time
 
-    while True:
-        if processed_frame is not None:
-            data = processed_frame
-            processed_frame = None
-            frame = data["image"]
+    stream = requests.get(RPI_IP, stream=True)
+    bytes = b''
+    for chunk in stream.iter_content(chunk_size=None):
+        bytes += chunk
+        a = bytes.find(b'\xff\xd8')
+        b = bytes.find(b'\xff\xd9')
+        if a != -1 and b != -1:
+            jpg = bytes[a:b+2]
+            bytes = bytes[b+2:]
+            frame = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
 
-            # Apply the FPS text to the frame
-            cv2.putText(frame, f'FPS: {fps}', (475, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 0), 2)
-            _, buffer = cv2.imencode('.jpg', frame)  # Encode frame as JPEG
+            # Predict the image
+            _, predicted_frame = predict_image(frame, postprocess=True, stream=True)
 
-            print(f"Total Processing latency: {(time.time() - data['time_received']) * 1000:.2f} ms")
+            ret, jpeg = cv2.imencode('.jpg', predicted_frame)
+            if ret:
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
 
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
-
-def frame_processor(raw=False):
-    global frame_count, fps, start_time, unprocessed_frame, processed_frame
-
-    while True:
-        if unprocessed_frame is not None:
-            frame_data = unprocessed_frame
-            unprocessed_frame = None
-            time_received = time.time()
+            # Update the frame count
             frame_count += 1
 
-            try:
-                # Decode the image from base64 and convert to OpenCV format
-                img_data = base64.b64decode(frame_data)
-                nparr = np.frombuffer(img_data, np.uint8)
-                img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-                if raw:
-                    if processed_frame is None:
-                        processed_frame = {"image": img, "time_received": time_received}
-                else:
-                    if img is not None:
-                        # Perform inference
-                        results = predict_image(img)
-                    else:
-                        print("Failed to decode frame")
-                    
-                    if processed_frame is None:
-                            processed_frame = {"image": results, "time_received": time_received}
-
-            except Exception as e:
-                print(f"Failed to process frame: {e}")
-
-            # Calculate FPS every second
+            # Get the current time
             current_time = time.time()
-            if current_time - start_time >= 1:
-                fps = round(frame_count / (current_time - start_time))
+
+            # Calculate elapsed time since last FPS calculation
+            elapsed_time = current_time - last_fps_time
+
+            if elapsed_time >= 1.0:
+                # Calculate FPS
+                fps = frame_count / elapsed_time
+                print(f"FPS: {fps:.2f}")
+
+                # Reset the frame count and last_fps_time
                 frame_count = 0
-                start_time = current_time
-
-class DebrisxNamespace(Namespace):
-    def on_connect(self):
-        print("Client connected")
-
-    def on_disconnect(self):
-        print("Client disconnected")
-
-    def on_start_latency_test(self, data):
-        self.emit_latency_test()
-
-    def on_send_frame(self, data):
-        global unprocessed_frame
-
-        payload = {
-            "client_sendtime" : data['client_sendtime'],
-        }
-
-        emit('frame_latency', payload, namespace='/debrisx')
-
-        if unprocessed_frame is None:
-            unprocessed_frame = data['frame']
-
-    def emit_latency_test(self):
-        # Send current time to the client and request immediate response
-        start_time = time.time()
-        emit('test_latency', {'server_time': start_time}, namespace='/debrisx')
-
-    def on_latency_response(self, data):
-        # Receive the latency response from the client with its timestamp
-        end_time = time.time()
-        server_time = data['server_time']
-
-        # Calculate the round trip latency
-        rtt = (end_time - server_time) * 1000
-
-        print(f"Round-trip time: {rtt:.2f} ms")
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+                last_fps_time = current_time
 
 @app.route('/')
-def index():
-    # Return a simple webpage with the video stream embedded
-    return Response('<html><body><img src="/video_feed"></body></html>')
+def video_predicted_feed():
+    return Response(predicted_stream(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
-# Register the namespace
-socketio.on_namespace(DebrisxNamespace('/debrisx'))
-
-# Start the processing thread
-thread = Thread(target=frame_processor, kwargs={'raw': True})
-thread.daemon = True
-thread.start()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
