bb05408bc053b836c3564704f7858d78341c35f4
nircniv <ng.ta.wei@outlook.com>
2024-07-30 20:10:11 +0800

    Initial Websocket implementation

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import socket
+import uvicorn
 from dotenv import load_dotenv
+
 load_dotenv(dotenv_path=".env", override=True)
 
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = "127.0.0.1"
+    finally:
+        s.close()
+    return ip
+
 if __name__ == "__main__":
-    import uvicorn
+    local_ip = get_local_ip()
+    print(f"Current local IP address: {local_ip}")
     uvicorn.run("server.app:app", host='0.0.0.0', port=5000, reload=True)
--- server/websockets/connection.py
+++ server/websockets/connection.py
@@ -1,7 +1,38 @@
-from fastapi import WebSocket
+import asyncio
+from fastapi import WebSocket, WebSocketDisconnect
+
+clients = {
+    "raspberry": [],
+    "frontend": []
+}
 
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+    client_type = websocket.query_params.get("client")
+
+    if client_type not in clients:
+        await websocket.close(code=1000, reason="Invalid client type")
+        return
+
+    clients[client_type].append(websocket)
+    client_ip = websocket.client.host  # Get the client IP address
+    print(f"Connection accepted from {client_ip} ({client_type})")
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+            print(f"Received message from {client_ip} ({client_type}): {data}")
+            target_client_type = "frontend" if client_type == "raspberry" else "raspberry"
+            await broadcast_message(f"Message text was: {data}", target_client_type)
+    except WebSocketDisconnect:
+        print(f"Connection closed from {client_ip} ({client_type})")
+    finally:
+        clients[client_type].remove(websocket)
+
+async def broadcast_message(message: str, target_client_type: str):
+    for client in clients[target_client_type]:
+        try:
+            await client.send_text(message)
+        except Exception as e:
+            print(f"Error sending message to client ({target_client_type}): {e}")
+            clients[target_client_type].remove(client)
